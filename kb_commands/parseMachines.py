7ff3a741eb4012d36b534d5a07b7442401351ace
Jeff Sabin <jsabin@proofpoint.com>
2015-03-17 09:57:30 -0600

    Added IP address to the list.

--- kb_commands/parseMachines.py
+++ kb_commands/parseMachines.py
@@ -15,12 +15,12 @@ decoded = json.loads(data)
 
 table = Texttable(0) # Set max_width to infinite
 table.set_deco(Texttable.HEADER)
-table.set_cols_dtype(['t', 't', 't', 't', 't'])
-table.set_cols_align(["l", "l", "l", "l", "l"])
+table.set_cols_dtype(['t', 't', 't', 't', 't', 't'])
+table.set_cols_align(["l", "l", "l", "l", "l", "l"])
 
-machines = [["Name", "Status", "Roles", "Environment", "JSON Tags"]]
+machines = [["Name", "IP", "Status", "Roles", "Environment", "JSON Tags"]]
 for machine in decoded:
-    machines.append([machine['fqdn'], machine['status'], machine['roles'], machine['environment_name'], machine['tags']])
+    machines.append([machine['fqdn'], machine['ip_address'], machine['status'], machine['roles'], machine['environment_name'], machine['tags']])
 
 table.add_rows(machines)
 print(table.draw())
